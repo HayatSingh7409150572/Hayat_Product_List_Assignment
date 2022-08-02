e3ffd25128319890aaf78e5c41b24a30615cd174
Hayat60403 <hayat.singh@ola.com>
2022-08-02 18:22:36 +0530

    product functionalities done

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,46 @@
 <template>
 <div class="content bg-slate-300">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+
+
+ <div class="border-solid border-2 border-black drop-shadow-md bg-gray-300">
+        <form >
+            <h1 style="color: black" class="font-bold text-3xl p-2">Shopping Product List</h1>
+            <table >
+                <tr class="text-center">
+                    <td><label>Product Name :</label></td>
+                    <td><input type="text" v-model="formdata.pname" class="bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 m-5"></td>
+                </tr>
+                <tr>
+                    <td><label>Product Price :</label></td>
+                    <td><input type="text" v-model="formdata.price" class="bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 m-5"></td>
+                </tr>
+                <tr>
+                    <td><label>Product Category:</label></td>
+                    <td><input type="text" v-model="formdata.category" class="bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 m-5"></td>
+                </tr>
+                <tr>
+                    <td><label>Product Color :</label></td>
+                    <td><input type="text" v-model="formdata.color" class="bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 m-5"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <button type="button" class=" border-solid font-extrabold rounded border-2 border-black p-3 bg-gray-300 bg-hover-black" @click="login">
+                           <NuxtLink to="/productForm"> Submit </NuxtLink>
+                        </button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+
+
+
+
+
+
+    
     <h1 class="text-center font-extrabold text-5xl mt-8 bg-blue-200">Product List</h1>
 
     <nav class="text-center font-bold text-xl mt-8 ">
@@ -8,9 +48,10 @@
             <select name="Category" id="jeans" class="bg-gray-300">
                 <option value="jeans"></option>
                 <option value="jeans">Regular</option>
-                <option value="jeans">Strechable</option>
+             
 
             </select>
+
             <select name="Category" id="Tshirt" class="bg-gray-300">
                 <option value="Tshirt">Bags</option>
                 <option value="Tshirt">Travelling</option>
@@ -46,7 +87,19 @@
                         <h3 class="font-bold text-xl">T-Shirt</h3>
                         <p><b>Round</b></p>
                         <h5><b>Rs 10,000/-only</b></h5>
+                        <!-- <p><input type="color"/>color</p> -->
+                    <div>
+                     <label for="color">Choose color:</label>
 
+<select name="Colors" id="color">
+  <option value="White">White</option>
+  <option value="Blue">Blue</option>
+  <option value="Red">Red</option>
+  <option value="Green">Green</option>
+</select>
+                      </div>
+
+                       <button></button>
                         <i class="fa fa-star checked"></i><i class="fa fa-star checked"></i><i class="fa fa-star checked"></i><br>
                         <button class="buy-1 border-2 px-2  border-black rounded-md">Buy Now</button>
                     </div>
@@ -57,6 +110,16 @@
                         <h3 class="font-bold text-xl">Tshirt</h3>
                         <p><b>V-Neck</b></p>
                         <h5><b>Rs 11,200/-only</b></h5>
+                                      <div>
+                     <label for="color">Choose color:</label>
+
+<select name="Colors" id="color">
+  <option value="White">White</option>
+  <option value="Blue">Blue</option>
+  <option value="Red">Red</option>
+  <option value="Green">Green</option>
+</select>
+                      </div>
                         <i class="fa fa-star checked"></i><i class="fa fa-star checked"></i><i class="fa fa-star checked"></i><br>
                         <button class="buy-2 border-2 px-2  border-black rounded-md">Buy Now</button>
                     </div>
@@ -69,6 +132,16 @@
                         <h3 class="font-bold text-xl">T-Shirt</h3>
                         <p><b>Chapri</b></p>
                         <h5><b>Rs 11,500/- only</b></h5>
+                                      <div>
+                     <label for="color">Choose color:</label>
+
+<select name="Colors" id="color">
+  <option value="White">White</option>
+  <option value="Blue">Blue</option>
+  <option value="Red">Red</option>
+  <option value="Green">Green</option>
+</select>
+                      </div>
                         <i class="fa fa-star checked"></i><i class="fa fa-star checked"></i><i class="fa fa-star checked"></i><br>
                         <button class="buy-3 border-2 px-2  border-black rounded-md">Buy Now</button>
                     </div>
@@ -79,6 +152,16 @@
                         <h3 class="font-bold text-xl">T-Shirt</h3>
                         <p><b>United colors oif Beneton</b></p>
                         <h5><b>Rs 13,200/- only</b></h5>
+                                      <div>
+                     <label for="color">Choose color:</label>
+
+<select name="Colors" id="color">
+  <option value="White">White</option>
+  <option value="Blue">Blue</option>
+  <option value="Red">Red</option>
+  <option value="Green">Green</option>
+</select>
+                      </div>
                         <i class="fa fa-star checked"></i><i class="fa fa-star checked"></i><i class="fa fa-star checked"></i><br>
                         <button class="buy-4 border-2 px-2  border-black rounded-md">Buy Now</button>
                     </div>
@@ -89,4 +172,59 @@
 
 </div>
 </template>
+
+
+<script>
+export default {
+    name: 'formdataproduct',
+    data() {
+        return {
+            myarr: [],
+            formdata: {
+                pname: "",
+                price: "",
+                category: "",
+                color: "",
+                image : "",
+            },
+        };
+    },
+    methods: {
+        createNewCard() {
+            this.myarr.push(this.formdata);
+        },
+        login(event) {
+            event.preventDefault();
+            if (this.isEdit == true) {
+                this.myarr[this.indexEdit] = this.formdata;
+                this.isEdit = false;
+                this.indexEdit = -1;
+            } else {
+                this.myarr.push(this.formdata);
+            }
+            this.formdata = {
+                id: 0,
+                pname: "",
+                price: "",
+                category: "",
+                color: "",
+                image : "",
+            }
+            console.log("Formdata is :", this.myarr);
+        },
+        deleteIndex(index) {
+            this.myarr.splice(index, 1);
+        },
+        editIndex(index) {
+            this.formdata.pname = this.myarr[index].pname;
+            this.formdata.price = this.myarr[index].price;
+            this.formdata.category = this.myarr[index].category;
+            this.formdata.color = this.myarr[index].color;
+            this.formdata.image = this.myarr[index].image;
+            this.isEdit = true;
+            this.indexEdit = index;
+        },
+    }
+}
+</script>
 
